57fb924a0a25123650de4ec0d3f5536ffb92d280
hayubi <hayubi53@gmail.com>
2025-04-15 14:48:30 +0500

    Update bothdraft1.cpp

--- bothdraft1.cpp
+++ bothdraft1.cpp
@@ -1,25 +1,70 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class User{
-    string name;
-    string email;
+class Seller;
+class Customer;
+
+class User
+{
+    protected:
+        string name;
+        int id;
+        string email;
+
     public:
-    User() : name(""), email(""){}
-    User(string n,string e): name(n), email(e){}
+        User() : name(""), id(0), email(""){}
+        User(string n, int ID, string e): name(n), id(ID), email(e) {}
 
-    virtual void Register(){
-        cout << "Enter your registration details\n";
-        cout << "Name: ";
-        getline(cin, name);
-        cout << "Email: ";
-        getline(cin, email);
-
-}
+        virtual void Register();
 };
-int main(){
-    User user;
-    user.Register();
-    return 0;
+
+
+class Seller : public User
+{
+    private:
+
+    public:
+        Seller() {}
+
+        void Register() override
+        {
+            cout << "cust";
+        }
+};
+
+
+class Customer : public User
+{
+    private:
+
+    public:
+        Customer() {}
+
+        void Register() override
+        {
+            cout << "cust";
+        }      
+};
+
+
+
+int main()
+{ 
+    Seller s;
+    Customer c;
+
+    int choice;
+    do
+    {
+        cout << "Are you interested in\n(1) Buying\n(2) Selling? ";
+        cin >> choice;
+        if (choice == 1)
+            c.Register();
+        else if (choice == 2)
+            s.Register();
+        else
+            cout << "Invalid choice. Please select option 1 or 2." << endl;
+    } while (choice != 1 && choice != 2);
+
 
 }
