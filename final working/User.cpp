cd0dbb434790f8c7ef85141455017882308bcd7b
hayubi <hayubi53@gmail.com>
2025-05-11 20:23:42 +0500

    Delete final working directory

--- final working/User.cpp
+++
@@ -1,8 +0,0 @@
-// user.cpp
-#include "user.h"
-#include <fstream>
-#include <string>
-
-User::User(const std::string& email, const std::string& password) : email(email), password(password) {}
-
-std::string User::getEmail() const { return email; }
