f7f277d67f80cddac4687d23ceffaa0fbec8e8d8
hayubi <hayubi53@gmail.com>
2025-05-12 20:55:42 +0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -3,5 +3,12 @@
 #include <string>
 using namespace std;
 
-User::User(const string& email, const string& password) : email(email), password(password) {}
+User::User(const string& n, const string& c, const string& add, const string& mail, const string& pass) : name(n), contact(c), address(add), email(mail), password(pass) {}
+
+string User::getname() const { return name; }
+string User::getcontact() const { return contact; }
+string User::getaddress() const { return address; }
 string User::getEmail() const { return email; }
+
+void User::setContact(string c) { contact = c; }
+void User::setAddress(string a) { address = a; }
